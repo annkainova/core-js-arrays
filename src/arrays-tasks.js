05c5c8d27751c0727ac90e1642ee5fde4feb64c3
Anna <cainowa@gmail.com>
2024-01-22 15:18:47 +0100

    feat: solve 1-2 task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,10 +20,14 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
-}
+function getIntervalArray(start, end) {
+  const initialValue = start;
+  const arrayLength = end - start + 1;
+  const arrEmpty = new Array(arrayLength);
+  const result = Array.from(arrEmpty, (e, index) => initialValue + index);
 
+  return result;
+}
 /**
  * Returns a new array where each element is the sum of the corresponding elements
  * from two arrays. Arrays can have different lengths.
@@ -37,10 +41,20 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
-}
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+  const newArr1 = [...arr1, ...Array(maxLength - arr1.length).fill(0)];
+  const newArr2 = [...arr2, ...Array(maxLength - arr2.length).fill(0)];
 
+  const arrEmpty = new Array(maxLength);
+
+  const result = Array.from(
+    arrEmpty,
+    (el, index) => newArr1[index] + newArr2[index]
+  );
+  return result;
+}
+sumArrays([], [1, 2, 3, 4]);
 /**
  * Returns an index of the specified element in array or -1 if element is not found.
  *
